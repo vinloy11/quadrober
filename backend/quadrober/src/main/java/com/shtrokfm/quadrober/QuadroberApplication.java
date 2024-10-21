ded5c9c8bd919b6fc9595d149d090eb56e91519f
ShtorkAO <a.shtork@yandex.ru>
2024-10-21 21:34:07 +0300

    Добавить Spring

---
+++ backend/quadrober/src/main/java/com/shtrokfm/quadrober/QuadroberApplication.java
@@ -0,0 +1,13 @@
+package com.shtrokfm.quadrober;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadroberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuadroberApplication.class, args);
+	}
+
+}
---
+++ backend/quadrober/src/test/java/com/shtrokfm/quadrober/QuadroberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shtrokfm.quadrober;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuadroberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
